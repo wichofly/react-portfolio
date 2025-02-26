97e6a706d4bec9eb31dd6c34255e5735c12d8e82
Mauricio Erazo <antonierazo13@hotmail.com>
2025-02-26 15:47:33 +0100

    refactor: App layout to use Flex component and update Home page with IconButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Box, Flex } from '@chakra-ui/react';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import StudyCase from './pages/StudyCase';
-import Footer from './components/Footer';
-import { Box } from '@chakra-ui/react';
-import { useColorModeValue } from './components/ui/color-mode';
 
 function App() {
-  const bgColor = useColorModeValue('gray.100', 'gray.900'); // light gray for light mode, darker gray for dark mode
-
   return (
-    <Box minH="100vh" bg={bgColor} display="flex" flexDirection="column">
+    <Flex minH="100vh" display="flex" flexDirection="column">
       <Router>
         <Navbar />
         <Box flex="1">
@@ -27,7 +24,7 @@ function App() {
         </Box>
         <Footer />
       </Router>
-    </Box>
+    </Flex>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { IconButton } from '@chakra-ui/react';
+import { RiHomeLine } from 'react-icons/ri';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <IconButton variant="ghost" aria-label="Home"  size="lg">
+      <RiHomeLine />
+    </IconButton>
+  );
+};
 
-export default Home
+export default Home;
