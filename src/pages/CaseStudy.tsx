c9afb2763ad1bf50181726a853b832bdc4732d9c
Mauricio Erazo <antonierazo13@hotmail.com>
2025-03-05 23:15:05 +0100

    feat: enhance CaseStudy component with additional features and technologies, update layout for better presentation

--- src/pages/CaseStudy.tsx
+++ src/pages/CaseStudy.tsx
@@ -1,9 +1,11 @@
+import { LinkButton } from '@/components/ui/link-button';
 import { projects } from '@/data/db';
-import { Box, Heading, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, SimpleGrid, Tag, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 
 const CaseStudy = () => {
-  const { id } = useParams(); // Get the project ID from the URL
+  const { id } = useParams(); // Get the project ID from the database
+  // console.log(useParams())
 
   const project = projects.find((p) => p.id === id);
 
@@ -12,42 +14,82 @@ const CaseStudy = () => {
   }
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} gap={6} mt="4">
-      <Box>
-        <Heading mb="4" size="2xl">
-          {project.caseStudy.title}
-        </Heading>
-        <Text fontSize="xl">{project.caseStudy.overview}</Text>
+    <>
+      <SimpleGrid columns={{ base: 1, md: 2 }} gap={6} mt="4">
+        <Box>
+          <Heading mb="4" size="2xl">
+            {project.caseStudy.title}
+          </Heading>
+          <Text fontSize="xl">{project.caseStudy.overview}</Text>
 
-        <Heading size="lg" mt={6}>
-          Key Features:
-        </Heading>
-        <Box display="flex" gap="4" flexWrap="wrap">
-          {project.caseStudy.features.map((feature, index) => (
-            <Text key={index} border="1.5px solid" p={1} rounded="md" mt="2">
-              {feature}
-            </Text>
-          ))}
+          <Heading size="lg" mt={6}>
+            Key Features:
+          </Heading>
+          <Box display="flex" gap="4" flexWrap="wrap">
+            {project.caseStudy.features.map((feature, index) => (
+              <Tag.Root
+                key={index}
+                size="lg"
+                colorPalette="teal"
+                variant="subtle"
+                mt="2"
+              >
+                <Tag.Label>{feature}</Tag.Label>
+              </Tag.Root>
+            ))}
+          </Box>
+
+          <Heading size="lg" mt={6}>
+            Technologies Used:
+          </Heading>
+          <Box display="flex" gap="4" flexWrap="wrap">
+            {project.caseStudy.toolsUsed.map((tool, index) => (
+              <Tag.Root
+                key={index}
+                size="lg"
+                colorPalette="teal"
+                variant="subtle"
+                mt="2"
+              >
+                {tool}
+              </Tag.Root>
+            ))}
+          </Box>
         </Box>
 
-        <Heading size="lg" mt={6}>
-          Technologies Used:
-        </Heading>
-        <Box display="flex" gap="4" flexWrap="wrap">
-          {project.caseStudy.toolsUsed.map((tool, index) => (
-            <Text key={index} border="1.5px solid" p={1} rounded="md" mt="2">
-              {tool}
-            </Text>
+        <Box columns={{ base: 1, md: 2 }}>
+          {project.caseStudy.images.map((img, index) => (
+            <Image key={index} src={img} alt={project.name} py={2} />
           ))}
         </Box>
-      </Box>
+      </SimpleGrid>
 
-      <Box columns={{ base: 1, md: 2 }}>
-        {project.caseStudy.images.map((img, index) => (
-          <Image key={index} src={img} alt={project.name} py={2} />
-        ))}
+      <Box display="flex" mt="10" justifyContent="space-between">
+        <Box>
+          <LinkButton
+            variant="outline"
+            colorPalette="teal"
+            href={project.github}
+            target="_blank"
+            mr="2"
+          >
+            Github
+          </LinkButton>
+          <LinkButton
+            variant="ghost"
+            colorPalette="teal"
+            href={project.deploy}
+            target="_blank"
+          >
+            Live Demo
+          </LinkButton>
+        </Box>
+
+        <LinkButton href="/projects" variant="outline" colorPalette="teal">
+          <Text>Go Back</Text>
+        </LinkButton>
       </Box>
-    </SimpleGrid>
+    </>
   );
 };
 
