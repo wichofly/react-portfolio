2ed55b891185567c43ea3ed69c7914e4220ba757
Mauricio Erazo <antonierazo13@hotmail.com>
2025-03-10 23:53:31 +0100

    feat: move tech skills and enjoy list to separate data file for better organization

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,50 +1,7 @@
-import { Box, Heading, SimpleGrid, Tag, Text } from '@chakra-ui/react';
-import { FaBrain, FaRunning } from 'react-icons/fa';
-import { FaMountainCity, FaPeopleRobbery } from 'react-icons/fa6';
-import { GiHiking, GiRead, GiSoccerBall } from 'react-icons/gi';
-import { MdOutlineComputer } from 'react-icons/md';
-import { TbBeach } from 'react-icons/tb';
+import { enjoyList, techSkills } from '@/data/skills';
+import { Box, Heading, Icon, SimpleGrid, Tag, Text } from '@chakra-ui/react';
 
 const About = () => {
-  const techSkills = [
-    'HTML5',
-    'CSS3',
-    'JavaScript',
-    'TypeScript',
-    'Node.js',
-    'Express',
-    'MongoDB',
-    'JWT (JSON Web Tokens)',
-    'Postman',
-    'RestAPI',
-    'React',
-    'React Query',
-    'Zustand',
-    'Zod',
-    'AWS Lambda',
-    'Redux',
-    'Test-Driven Development (TDD)',
-    'Behavior-Driven Development (BDD)',
-    'Google Firebase',
-    'GIT',
-    'Github',
-    'Responsive Design',
-    'React Bootstrap',
-    'Chakra UI',
-    'Tailwind CSS',
-  ];
-
-  const enjoyList = [
-    { element: <GiSoccerBall />, label: 'Playing soccer' },
-    { element: <MdOutlineComputer />, label: 'Programming' },
-    { element: <FaBrain />, label: 'Learning new skills' },
-    { element: <FaPeopleRobbery />, label: 'Getting to know people' },
-    { element: <FaMountainCity />, label: 'Getting to know new places' },
-    { element: <GiHiking />, label: 'Hiking' },
-    { element: <FaRunning />, label: 'Jogging' },
-    { element: <GiRead />, label: 'Reading' },
-    { element: <TbBeach />, label: 'Going to the beach, rivers and lakes' },
-  ];
   return (
     <>
       <Box textAlign="center">
@@ -134,7 +91,9 @@ const About = () => {
                 variant="subtle"
                 mt="2"
               >
-                <Tag.StartElement>{list.element}</Tag.StartElement>
+                <Tag.StartElement>
+                  <list.element />
+                </Tag.StartElement>
                 <Tag.Label>{list.label}</Tag.Label>
               </Tag.Root>
             ))}
