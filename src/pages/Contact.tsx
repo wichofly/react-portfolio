1aa93afbb62069ae3a18ef41d0015aa96d8ff469
Mauricio Erazo <antonierazo13@hotmail.com>
2025-03-12 15:34:57 +0100

    feat: enhance Contact page with improved layout and email link for better engagement

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,31 @@
+import { Box, Heading, HStack, Link, Text } from '@chakra-ui/react';
+import { BsEnvelopeAt } from 'react-icons/bs';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <Box textAlign="center">
+      <Heading fontSize="3xl">Let's Connect!</Heading>
+      <Text fontSize="xl" mt="2">
+        I'm always open to new opportunities, collaborations, and discussions.
+      </Text>
+      <Text fontSize="xl" mt="1">
+        Whether you have a question, a project, or just want to say hi, feel
+        free to reach out!
+      </Text>
 
-export default Contact
+      <HStack mt="12" display="flex" justifyContent="center">
+        <Link
+          href="mailto:antonierazo13@gmail.com"
+          colorPalette="teal"
+          fontSize="8xl"
+          _hover={{ color: 'teal.600' }}
+          transition="0.3s ease-in-out"
+        >
+          <BsEnvelopeAt />
+        </Link>
+      </HStack>
+    </Box>
+  );
+};
+
+export default Contact;
