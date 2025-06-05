e4b81762a25abc553047c8fac1338c2e3596a00b
Mauricio Erazo <antonierazo13@hotmail.com>
2025-06-06 00:24:37 +0200

    feat: add drink recipes section with images and project details

--- src/components/ProjectIcons.tsx
+++ src/components/ProjectIcons.tsx
@@ -24,6 +24,7 @@ import {
   SiRender,
   SiTypescript,
   SiZod,
+  SiReactrouter,
 } from 'react-icons/si';
 
 const iconMap: { [key: string]: IconType } = {
@@ -47,6 +48,7 @@ const iconMap: { [key: string]: IconType } = {
   Vercel: IoLogoVercel,
   Zod: SiZod,
   TailwindCss: RiTailwindCssFill,
+  ReactRouter: SiReactrouter,
 };
 
 interface ProjectIconsProps {
